efc2dc4256be57bd91a0ea0fbb03b93b1e6e2a7b
ohenaoz <89320661+ohenaoz@users.noreply.github.com>
2021-12-08 16:08:00 -0500

    commit

--- master.sh
+++ master.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
+aws_access_key_id='ASIAWN43HLCG74CNUJ3U'
+aws_secret_access_key='398fcyNjlcirRDEgTCU66ktcom8SjJYQFPPk6lWJ'
+aws_session_token='FwoGZXIvYXdzEP7//////////wEaDE880D62dXieheiGwCKuARC3grsGab+6hQZ0XF504oNLKgjymX1HPwFwhwNt1ToTBFFvPVsH8LLlZx8R9zrYYdN3uwQaFNEyH5FZMIgTECJLijU97psIOMZOt4btGGsX6AR1Q0WeLhr7uGH6gyXsyJgW+Ykg/YSpC1lokSsUNtJAlj5s0oZ4QEJjzTt2c0tw/xojwKyBIsF8wOmDrH8u63my6L/4cg1uEHolwV0MmtzbtERbsbkGO8qp+9d/NyjyrsSNBjItyWW5bTCffZRXvtSX8b6zHHO3a0Z7nAMtlri0GjRQtkdxkkG2TBDzAgxoSJsG'
 
-aws_access_key_id='ASIAWN43HLCG4N5AT3OC'
-aws_secret_access_key='LJVnoz4hYZMJkzpGHNOmM5HhYyNSIknxhxL35i3y'
-aws_session_token='FwoGZXIvYXdzEND//////////wEaDLQi81goWs+SOKQpuCKuAacIqlKGQbpFKEJv1k14zCRTMHDrVnvLFSJUferhh3zlxSH7kDvNMWvgHOW2p3EkNN+Pt3rgUCRZht5niYfRiJif92UOEFqOq2vw/eW2kDFiUij/ZoFJDOlPC4NI9g2Th7LOXmlPYQr6rCsAuqigpbo670dPj2jwwubiO35cJFfzLHQffkpU6pW+hl57jhMiS+WkA0enRPGv0ODuMgF12MrGIcOe91UvtalSwEO7OCjgpbqNBjIthoK31hHe3Sp6Vnjx0Ozxo+U0k0laLoYtVTBi7J9x1iyNagBI/mpKlMJswt7D'
-
-
-#python3 /home/ahenao/scripts/PI/EMR_Cluster.py --key1 $aws_access_key_id --key2 $aws_secret_access_key --key3 $aws_session_token
-python3 /home/ahenao/scripts/PI/Clustering.py --key1 $aws_access_key_id --key2 $aws_secret_access_key --key3 $aws_session_token
+python3 /home/ahenao/scripts/PI/EMR_Cluster.py --key1 $aws_access_key_id --key2 $aws_secret_access_key --key3 $aws_session_token
